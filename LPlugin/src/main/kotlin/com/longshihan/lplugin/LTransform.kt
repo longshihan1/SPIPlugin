66c396400b0d5c2e31cb5a58f44cb6f1e6feb891
longhe <577093937@qq.com>
2021-03-17 22:55:01 +0800

    [fix] 添加jar解析

--- LPlugin/src/main/kotlin/com/longshihan/lplugin/LTransform.kt
+++ LPlugin/src/main/kotlin/com/longshihan/lplugin/LTransform.kt
@@ -14,11 +14,11 @@ import org.gradle.api.Project
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassReader.EXPAND_FRAMES
 import org.objectweb.asm.ClassWriter
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
 import java.util.*
+import java.util.jar.JarFile
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
 
 /**
  * Created by longhe on 2020-07-16.
@@ -44,9 +44,9 @@ open class LTransform(val project: Project) : Transform() {
 
     override fun transform(transformInvocation: TransformInvocation?) {
         super.transform(transformInvocation)
-        if (!Config.enable){
+        if (!Config.enable) {
             println("时间插桩插件关闭")
-        }else{
+        } else {
             println("时间插桩插件开启")
         }
         try {
@@ -66,63 +66,116 @@ open class LTransform(val project: Project) : Transform() {
             val outputProvider = transformInvocation?.outputProvider
             inputs?.stream()?.forEach { input ->
                 input.jarInputs.stream().forEach { jarInput ->
-                    val dest = outputProvider?.getContentLocation(
-                        jarInput.file.absolutePath,
-                        jarInput.contentTypes, jarInput.scopes, Format.JAR
-                    )
-                    //将修改过的字节码copy到dest，就可以实现编译期间干预字节码的目的了
-                    FileUtils.copyFile(jarInput.file, dest)
+                    traceJarFiles(jarInput, outputProvider)
                 }
                 input.directoryInputs.stream().forEach { directoryInput ->
-                    val fileCPath=directoryInput.file.absolutePath
-                    if (directoryInput.file.isDirectory) {
-                        if (directoryInput.file.name != "META-INF") {
-                            directoryInput.file.walk().forEach { file ->
-                                val name = file.name
-                                val dpath=file.absolutePath
-                                val currentPath=dpath.replace(fileCPath,"")
-                                    .replace("/",".")
-                                    .replaceFirst(".","")
-                                println("---------$dpath")
-                                var isReplce=false
-                                for (configStr in LConfig.excludes){
-                                    if (currentPath.startsWith(configStr)){
-                                        isReplce=true
-                                        break
-                                    }
-                                }
-                                if (!isReplce &&name.endsWith(".class") && !name.startsWith("R\$")
-                                    && "R.class" != name && "BuildConfig.class" != name) {
-                                    try {
-                                        println("$currentPath:is changeing ,,,,")
-                                        val cr = ClassReader(file.readBytes())
-                                        val cw = ClassWriter(cr,ClassWriter.COMPUTE_MAXS)
-                                        val cv = TestMethodClassAdapter(currentPath,cw)
-                                        cr.accept(cv, EXPAND_FRAMES)
-                                        val code = cw.toByteArray()
-                                        val fos =
-                                            FileOutputStream(file.parentFile.absolutePath + File.separator + name)
-                                        fos.write(code)
-                                        fos.close()
-                                    } catch (e: Exception) {
-                                        e.printStackTrace()
-                                    }
-                                }
-                            }
-                        }
-                        val dest = outputProvider?.getContentLocation(
-                            directoryInput.name,
-                            directoryInput.contentTypes,
-                            directoryInput.scopes,
-                            Format.DIRECTORY
-                        )
-                        println("----directory is location  is  ${dest?.absolutePath}")
-                        FileUtils.copyDirectory(directoryInput.file, dest)
-                    }
+                    traceFiles(directoryInput, outputProvider)
                 }
             }
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
+
+    fun traceFiles(directoryInput: DirectoryInput, outputProvider: TransformOutputProvider?) {
+        val fileCPath = directoryInput.file.absolutePath
+        directoryInput.file.walk().forEach { file ->
+            val name = file.name
+            val dpath = file.absolutePath
+            var currentPath = dpath.replace(fileCPath + File.separator, "")
+            println("ER:$currentPath")
+            currentPath = transformPath(currentPath, "\\")
+            if (checkPath(currentPath)&&file.isFile) {
+                try {
+                    println("$currentPath:is changeing ,,,,")
+                    val fileInputStream = FileInputStream(file)
+                    val code = tranformBtye(fileInputStream, currentPath)
+                    val fos = FileOutputStream(
+                        file.parentFile.absolutePath + File.separator + name
+                    )
+                    fos.write(code)
+                    fos.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+        }
+        val dest = outputProvider?.getContentLocation(
+            directoryInput.name,
+            directoryInput.contentTypes,
+            directoryInput.scopes,
+            Format.DIRECTORY
+        )
+        println("----directory is location  is  ${dest?.absolutePath}")
+        FileUtils.copyDirectory(directoryInput.file, dest)
+    }
+
+    fun traceJarFiles(jarInput: JarInput, outputProvider: TransformOutputProvider?) {
+        if (jarInput.file.absolutePath.endsWith(".jar")) {
+            var jarName = jarInput.name
+            val md5Name = DigestUtils.md5Hex(jarInput.file.absolutePath)
+            if (jarName.endsWith(".jar")) {
+                jarName = jarName.substring(0, jarName.length - 4)
+            }
+            val jarFile = JarFile(jarInput.file)
+            val enumeration = jarFile.entries()
+            val tempFile = File(jarInput.file.parent + File.separator + "classes_temp.jar")
+            if (tempFile.exists()) {
+                tempFile.delete()
+            }
+            val jarOutputStream = JarOutputStream(FileOutputStream(tempFile))
+            while (enumeration.hasMoreElements()) {
+                val jarEntry = enumeration.nextElement()
+                var entryName = jarEntry.name
+                val zipEntry = ZipEntry(entryName)
+                val inputStream = jarFile.getInputStream(jarEntry)
+                println("-=--=-===---$entryName")
+                entryName = transformPath(entryName, "/")
+                if (checkPath(entryName)) {//匹配class文件名
+                    println("$entryName:os changeing ,,,,")
+                    jarOutputStream.putNextEntry(zipEntry)
+                    jarOutputStream.write(tranformBtye(inputStream, entryName))
+                } else {
+                    jarOutputStream.putNextEntry(zipEntry)
+                    jarOutputStream.write(inputStream.readBytes())
+                }
+                jarOutputStream.closeEntry()
+            }
+            jarOutputStream.close()
+            jarFile.close()
+            //处理完输出给下一任务作为输入
+            val dest = outputProvider?.getContentLocation(
+                jarName + md5Name,
+                jarInput.contentTypes, jarInput.scopes, Format.JAR
+            )
+            FileUtils.copyFile(tempFile, dest)
+            tempFile.delete()
+        }
+    }
+
+    fun tranformBtye(inputStream: InputStream, currentPath: String): ByteArray {
+        val cr = ClassReader(inputStream)
+        val cw = ClassWriter(cr, ClassWriter.COMPUTE_MAXS)
+        val cv = TestMethodClassAdapter(currentPath, cw)
+        cr.accept(cv, EXPAND_FRAMES)
+        return cw.toByteArray()
+    }
+
+    fun checkPath(path: String): Boolean {
+        if (path.endsWith(".class") && !path.contains("R$")
+            && !path.endsWith("R.class") && !path.endsWith("BuildConfig.class")
+            && !path.contains("META-INF")
+        ) {
+            return true
+        }
+        return false
+    }
+
+    fun transformPath(path: String, sequter: String): String {
+        val currentPath = path.replace(sequter, ".")
+        println(currentPath)
+        return currentPath
+    }
+
 }
