05b16093f72007dc1e79d60523d1a165cd980904
he.long <helong.lh@alibaba-inc.com>
2021-05-06 19:56:24 +0800

    修改过滤插桩方法

--- LPlugin/src/main/kotlin/com/longshihan/lplugin/TestMethodClassAdapter.kt
+++ LPlugin/src/main/kotlin/com/longshihan/lplugin/TestMethodClassAdapter.kt
@@ -1,15 +1,29 @@
 package com.longshihan.lplugin
 
-import com.longshihan.lplugin.utils.Config
-import com.longshihan.lplugin.utils.UUIDUtils
-import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.Opcodes.ASM5
-import org.objectweb.asm.commons.AdviceAdapter
+
 
 class TestMethodClassAdapter (val owner:String,var classVisitor: ClassVisitor):ClassVisitor(ASM5,classVisitor),Opcodes{
+    private var isABSClass = false
+    private var className: String? = null
+    override fun visit(
+        version: Int,
+        access: Int,
+        name: String?,
+        signature: String?,
+        superName: String?,
+        interfaces: Array<out String>?
+    ) {
+        super.visit(version, access, name, signature, superName, interfaces)
+        className=name
+        if (access and Opcodes.ACC_ABSTRACT > 0 || access and Opcodes.ACC_INTERFACE > 0) {
+            isABSClass = true
+            println("ABClass--$name")
+        }
+    }
     override fun visitMethod(
         access: Int,
         name: String?,
@@ -17,42 +31,52 @@ class TestMethodClassAdapter (val owner:String,var classVisitor: ClassVisitor):C
         signature: String?,
         exceptions: Array<out String>?
     ): MethodVisitor? {
-        var mv = super.visitMethod(access, name, descriptor, signature, exceptions);
-        mv = object : AdviceAdapter(Opcodes.ASM5, mv, access, name, descriptor) {
-            var uuidClassName:String?=null;
-            override fun onMethodEnter() {
-                for (content in Config.blackMethodList){
-                    if (name==content){
-                        super.onMethodEnter()
-                        return
-                    }
-                }
-                uuidClassName=UUIDUtils.transformUUIDformClass(owner+name)
-                mv.visitLdcInsn(owner)
-                mv.visitLdcInsn(name)
-                mv.visitLdcInsn(uuidClassName)
-                mv.visitMethodInsn(INVOKESTATIC, "com/longshihan/collect/init/Trace", "initFirst",
-                    "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
-                    false
-                )
-            }
 
-            override fun onMethodExit(opcode: Int) {
-                for (content in Config.blackMethodList){
-                    if (name==content){
-                        super.onMethodEnter()
-                        return
+            var mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+        if (isABSClass){
+            return mv
+        }else {
+//            return TraceMethodAdapter(className,access,descriptor,signature,exceptions)
+            mv = object : AdviceAdapter(Opcodes.ASM5, mv, access, name, descriptor) {
+                var uuidClassName: String? = null;
+                override fun onMethodEnter() {
+                    for (content in Config.blackMethodList) {
+                        if (name == content) {
+                            super.onMethodEnter()
+                            return
+                        }
                     }
+                    uuidClassName = UUIDUtils.transformUUIDformClass(owner + name)
+                    mv.visitLdcInsn(owner)
+                    mv.visitLdcInsn(name)
+                    mv.visitLdcInsn(uuidClassName)
+                    mv.visitMethodInsn(
+                        INVOKESTATIC, "com/longshihan/collect/init/Trace", "initFirst",
+                        "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
+                        false
+                    )
                 }
-                mv.visitLdcInsn(owner)
-                mv.visitLdcInsn(name)
-                mv.visitLdcInsn(uuidClassName)
-                mv.visitMethodInsn(
-                    Opcodes.INVOKESTATIC, "com/longshihan/collect/init/Trace", "initLast",
-                    "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", false);
+
+                override fun onMethodExit(opcode: Int) {
+                    for (content in Config.blackMethodList) {
+                        if (name == content) {
+                            super.onMethodEnter()
+                            return
+                        }
+                    }
+                    mv.visitLdcInsn(owner)
+                    mv.visitLdcInsn(name)
+                    mv.visitLdcInsn(uuidClassName)
+                    mv.visitMethodInsn(
+                        Opcodes.INVOKESTATIC, "com/longshihan/collect/init/Trace", "initLast",
+                        "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", false
+                    );
+                }
+
+
             }
+            return mv
         }
-        return mv
     }
 
 }
